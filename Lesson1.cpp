6ef79aa4c0a8284daaf7b47c5bae0d3b5e4a4b79
GreezA <91843390+Grizoglazov@users.noreply.github.com>
2026-04-07 19:41:16 +0300

    ДЗ №1

---
+++ Lesson1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+int main() {
+	
+	std::cout << "\tHello World!!\n";
+	std::cout << "My name is Andrew!\n";
+	std::cout << "I'm 56 years old." << std::endl;
+	std::cout << "I'm very old," << " but i'm student!!!"
+			  << " I hope, i can do it!!))))\n" 
+			  << "See you next time!!!\n\n";
+}
